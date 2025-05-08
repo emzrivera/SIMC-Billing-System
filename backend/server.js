5afa0ed812552183d5809059ac77b36de54513e6
Ela Mariz Rivera <elamarizrivera@gmail.com>
2025-05-08 15:09:50 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -50,14 +50,6 @@ app.get('/api', (req, res) => {
   res.send('API is running...');
 });
 
-
-app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
-});
-
-
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
